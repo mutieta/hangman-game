3250da0a83a211d9401657feca3a38e776ba00e8
Sanna Nan <130217943+mutieta@users.noreply.github.com>
2023-11-26 00:18:01 +0700

    Add files via upload

--- hangman.cpp
+++ hangman.cpp
@@ -18,6 +18,69 @@ void DrawMan(int tries){
 }
 
 int main(){
+    srand(time(NULL));
+    string wordList[5] = {"apple","peach","banana","mango","strawberry"};
+    string word;
+    string guessed;
+    
+    word = wordList[rand()%5];
 
+    int wordLength = word.length();
+    string disWord (wordLength, '_');
+
+    int found = 0;
+    char guess = ' ';
+    int tries = 5; 
+    int flagFound = 0;
+
+    while(tries>=0){
+        system("cls");
+        cout<<"Guess Fruit Name: "<<endl<<endl;
+
+        for(int i=0; i<wordLength; i++)
+            cout<<" "<<disWord[i]<<" ";
+        cout<<endl<<endl<<endl;
+
+        cout<<"Wrong Attempts: "<<tries<<" / "<<WRONGTRIES<<endl;
+        cout<<"Guessd Letters: "<<guessed<<endl;
+
+        DrawMan(tries);
+
+        if( found == wordLength){
+            cout<<endl;
+            cout<<"****************"<<endl;
+            cout<<"*   YOU WIN    *"<<endl;
+            cout<<"****************"<<endl;
+            break;
+        }
+
+        if( tries == 0) break;
+
+        cout<<"Pick a Letter: ";
+        guess = getche();
+
+        guessed = guess + " " + guess;
+
+        if( disWord.find(guess) != string::npos ) tries++;
+
+        flagFound = 0;
+        for (int i=0; i<wordLength; i++){
+            if( word[i]==guess && disWord[i]=='_'){
+                disWord[i] = guess;
+                found++;
+                flagFound = 1;
+            }
+        }
+        if( !flagFound )
+            tries--;
+    }
+    if( found != wordLength ){
+        cout<<endl;
+        cout<<"****************"<<endl;
+        cout<<"*   YOU LOSE   *"<<endl;
+        cout<<"****************"<<endl;
+    }
+    
+    getch();
     return 0;
 }
