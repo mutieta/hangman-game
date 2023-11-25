f1641baee3b324d6994f9ad368cc513759c1a85d
Sanna Nan <130217943+mutieta@users.noreply.github.com>
2023-11-25 23:55:51 +0700

    Add files via upload

--- hangman.cpp
+++ hangman.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <conio.h>
+#include <string>
+#include <stdlib.h>
+#include <ctime>
 
+#define WRONGTRIES 5
 using namespace std;
+
+void DrawMan(int tries){
+    cout<<"  -----"<<endl;
+    cout<<"  |   |"<<endl;
+    cout<<"  |"; if(tries>=1) cout<<"   o   "; cout<<endl;
+    cout<<"  |"; if(tries>=3) cout<<"  /|\\  "; cout<<endl;
+    cout<<"  |"; if(tries>=5) cout<<"  / \\   "; cout<<endl;
+    cout<<"  |"<<endl;
+    cout<<"__|_"<<endl;
+}
 
 int main(){
 
